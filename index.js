4b10facd674d1c8437700dae568614ad41b26fc4
Mikayla <user@users-MBP.attlocal.net>
2023-06-14 11:44:01 -0400

    created function for possible shapeelements and fill color

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const inquirer = require('inquirer');
 const { SVG, Path } = require('svg-builder');
+
+function generateLogo(textColor, text, shape, shapeColor) {
+    const svg = new SVG().size(300, 200);
+
+    let shapeElement;
+    if (shape === 'circle') {
+      shapeElement = svg.circle(150, 100, 50);
+    } else if (shape === 'triangle') {
+      shapeElement = svg.polygon([[150, 50], [250, 150], [50, 150]]);
+    } else if (shape === 'square') {
+      shapeElement = svg.rect(100, 50, 100, 100);
+    }
+
+    shapeElement.fill(shapeColor);
+}
